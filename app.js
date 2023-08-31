b209e7a21a84f75ae93e0d2e359b49f8eee4b166
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-08-31 20:38:07 -0300

    basic routing

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const router = require('./routes/routes');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app.use(router);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+    console.log(`App listening on port 3000`);
 });
