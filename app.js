c5bed310e265357aa57ddc761ee55b3ba06bf3c4
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-09-01 11:27:47 -0300

    handlebars view setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+
 const app = express();
 const router = require('./routes/routes');
+
+app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: false }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 app.use(router);
 
