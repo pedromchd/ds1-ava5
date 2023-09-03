084c3d68ffd9e39cd522d8e6aeef456b946c2cdd
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-09-02 23:02:43 -0300

    handlebars prototype access

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const router = require('./routes/router');
-const sequelize = require('./database/config');
+const sequelize = require('./config/database');
 
 sequelize.sync();
 
 const app = express();
 
-app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: false }));
+app.engine('.hbs', engine({ 
+    extname: '.hbs', 
+    defaultLayout: false, 
+    handlebars: allowInsecurePrototypeAccess(handlebars)
+}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
