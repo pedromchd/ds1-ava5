9a0faf32baf19cdda23db2281f2f98ecfa4d0fe1
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-01 16:17:46 -0300

    renaming controller

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const myController = require('../controllers/myController');
+const livroController = require('../controllers/livroController');
 
 const router = express.Router();
 
-router.get('/', myController.home);
-router.get('/new', myController.new);
-router.post('/new', myController.create);
-router.get('/edit/:id', myController.edit)
-router.post('/edit/:id', myController.update);
-router.get('/delete/:id', myController.delete);
+router.get('/', livroController.home);
+router.get('/new', livroController.new);
+router.post('/new', livroController.create);
+router.get('/edit/:id', livroController.edit)
+router.post('/edit/:id', livroController.update);
+router.get('/delete/:id', livroController.delete);
 
 module.exports = router;
