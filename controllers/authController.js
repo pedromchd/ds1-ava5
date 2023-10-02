364181a10f4a45b89a33883b3e71187e188fc80e
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-02 11:47:07 -0300

    validação do login e do cadastro

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,30 +7,45 @@ exports.login = (req, res) => {
 };
 
 exports.auth = async (req, res) => {
-    const { email, senha } = req.body;
-    const { id, nome, senha: hash } = await User.findOne({ where: { email } });
-    const auth = await bcrypt.compare(senha, hash);
-    if (!auth) {
-        req.flash('errors', 'Houve um erro na autenticação');
+    const result = validationResult(req).formatWith(err => err.msg);
+    if (!result.isEmpty()) {
+        req.flash('errors', result.array());
         return res.redirect('back');
     }
-    req.session.regenerate(() => {
-        req.session.user = { id, nome };
-        return res.redirect('/');
-    });
+    const { email, senha } = req.body;
+    try {
+        const { id, nome, senha: hash } = await User.findOne({ where: { email } });
+        const auth = await bcrypt.compare(senha, hash);
+        if (!auth) {
+            req.flash('errors', 'A senha está incorreta.');
+            return res.redirect('back');
+        }
+        req.session.regenerate(() => {
+            req.session.user = { id, nome };
+            return res.redirect('/');
+        });
+    } catch {
+        req.flash('errors', 'O email informado não existe.');
+        return res.redirect('back');
+    };
 };
 
 exports.cadastro = (req, res) => {
     return res.render('auth/cadastro', { title: 'Cadastro' });
 };
 
-exports.create = async (req, res) => {
+exports.register = async (req, res) => {
     const result = validationResult(req).formatWith(err => err.msg);
     if (!result.isEmpty()) {
         req.flash('errors', result.array());
         return res.redirect('back');
     }
     const { nome, email, senha } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+        req.flash('errors', 'O email informado já existe.');
+        return res.redirect('back');
+    }
     const hash = await bcrypt.hash(senha, 10);
     await User.create({ nome, email, senha: hash });
     req.flash('success', 'Usuário cadastrado com sucesso!');
@@ -41,4 +56,4 @@ exports.logout = (req, res) => {
     req.session.destroy(() => {
         return res.redirect('/login');
     });
-}
+};
--- routes/router.js
+++ routes/router.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const { body, checkSchema } = require('express-validator');
+const { checkSchema } = require('express-validator');
 const authController = require('../controllers/authController');
 const livroController = require('../controllers/livroController');
-const User = require('../models/User');
 
 const router = express.Router();
 
 router.get('/login', authController.login);
-router.post('/login', authController.auth);
+router.post('/login', checkSchema({
+    email: { trim: true, isEmail: { errorMessage: 'Insira um email válido.' } },
+    senha: { notEmpty: { errorMessage: 'Insira uma senha válida.' } }
+}), authController.auth);
 router.get('/cadastro', authController.cadastro);
-router.post('/cadastro', body('email').custom(
-    async (email) => {
-        const user = await User.findOne({ where: { email } });
-        if (user) {
-            throw new Error('Email já cadastrado no sistema.');
-        }
-    }
-), authController.create);
+router.post('/cadastro', checkSchema({
+    nome: { trim: true, notEmpty: { errorMessage: 'Insira um nome válido.' } },
+    email: { trim: true, isEmail: { errorMessage: 'Insira um email válido.' } },
+    senha: { notEmpty: { errorMessage: 'Insira uma senha válida.' } }
+}), authController.register);
 router.get('/logout', authController.logout);
 
 router.get('/', livroController.home);
