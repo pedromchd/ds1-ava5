c93cda3beb2b380cb9ededa7d83da9cce28782e9
pedromchd <pedrogmachado28@outlook.com>
2023-10-08 07:24:14 -0300

    routing and controllers validation

--- controllers/livroController.js
+++ controllers/livroController.js
@@ -1,38 +1,49 @@
+const { validationResult } = require('express-validator');
 const Livro = require('../models/Livro');
 
 exports.home = async (req, res) => {
     const results = await Livro.findAll();
-    return res.render('admin/livro/home', { title: 'Página inicial', results });
+    return res.render('admin/livros/home', { title: 'Página inicial', results });
 };
 
 exports.new = (req, res) => {
-    return res.render('admin/livro/new', { title: 'Adicionar livro' })
+    return res.render('admin/livros/new', { title: 'Adicionar livro' })
 };
 
 exports.create = async (req, res) => {
+    const result = validationResult(req).formatWith(err => err.msg);
+    if (!result.isEmpty()) {
+        req.flash('errors', result.array());
+        return res.redirect('back');
+    }
     const { titulo, ano, autor, editora, quantidade } = req.body;
     const { id } = await Livro.create({ titulo, ano, autor, editora, quantidade });
     req.flash('success', 'Livro adicionado com sucesso!');
-    return res.redirect(`/admin/#${id}`);
+    return res.redirect(`/admin/livros#${id}`);
 };
 
 exports.edit = async (req, res) => {
     const id = req.params.id;
     const result = await Livro.findByPk(id);
-    return res.render('admin/livro/edit', { title: 'Editar livro', result });
+    return res.render('admin/livros/edit', { title: 'Editar livro', result });
 };
 
 exports.update = async (req, res) => {
+    const result = validationResult(req).formatWith(err => err.msg);
+    if (!result.isEmpty()) {
+        req.flash('errors', result.array());
+        return res.redirect('back');
+    }
     const id = req.params.id;
     const { titulo, ano, autor, editora, quantidade } = req.body;
     await Livro.update({ titulo, ano, autor, editora, quantidade }, { where: { id } });
     req.flash('success', 'Livro atualizado com sucesso!');
-    return res.redirect(`/admin/#${id}`);
+    return res.redirect(`/admin/livros#${id}`);
 };
 
 exports.delete = async (req, res) => {
     const id = req.params.id;
     await Livro.destroy({ where: { id } });
     req.flash('success', 'Livro deletado com sucesso!');
-    return res.redirect('/admin/');
+    return res.redirect('/admin/livros');
 };
--- routes/router.js
+++ routes/router.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { checkSchema } = require('express-validator');
 const authController = require('../controllers/authController');
 const livroController = require('../controllers/livroController');
+const userController = require('../controllers/userController');
 const libController = require('../controllers/libController');
 
 const router = express.Router();
 
-// Library homepage
-router.get('/', (req, res) => res.redirect('/home'));
-router.get('/home', libController.home);
+// Biblioteca homepage
+router.get('/', libController.home);
 
 // Login and register
 router.get('/login', authController.login);
@@ -24,18 +24,48 @@ router.post('/cadastro', checkSchema({
 }), authController.register);
 router.get('/logout', authController.logout);
 
-// Admin book CRUD
-router.get('/admin', (req, res) => res.redirect('/admin/home'));
-router.all('/admin/*', (req, res, next) => {
+// Admin homepage
+router.all('/admin(/*)?', (req, res, next) => {
     if (!req.session.user) { return res.status(401).redirect('/login'); }
-    if (req.session.user.id !== 1) { return res.status(403).redirect('/login'); }
+    if (req.session.user.id !== 1) { return res.status(403).redirect('/'); }
     next();
 });
-router.get('/admin/home', livroController.home);
-router.get('/admin/new', livroController.new);
-router.post('/admin/new', livroController.create);
-router.get('/admin/edit/:id', livroController.edit)
-router.post('/admin/edit/:id', livroController.update);
-router.get('/admin/delete/:id', livroController.delete);
+router.get('/admin(/)?', (req, res) => res.render('admin/home'));
+
+// Admin livro CRUD
+router.get('/admin/livros(/)?', livroController.home);
+router.get('/admin/livros/new', livroController.new);
+router.post('/admin/livros/new', checkSchema({
+    titulo: { trim: true, notEmpty: { errorMessage: 'Insira um título válido.' } },
+    ano: { trim: true, notEmpty: { errorMessage: 'Insira um ano válido.' } },
+    autor: { trim: true, notEmpty: { errorMessage: 'Insira um autor válido.' } },
+    editora: { trim: true, notEmpty: { errorMessage: 'Insira uma editora válida.' } },
+    quantidade: { trim: true, notEmpty: { errorMessage: 'Insira uma quantidade válida.' } }
+}), livroController.create);
+router.get('/admin/livros/edit/:id', livroController.edit)
+router.post('/admin/livros/edit/:id', checkSchema({
+    titulo: { trim: true, notEmpty: { errorMessage: 'Insira um título válido.' } },
+    ano: { trim: true, notEmpty: { errorMessage: 'Insira um ano válido.' } },
+    autor: { trim: true, notEmpty: { errorMessage: 'Insira um autor válido.' } },
+    editora: { trim: true, notEmpty: { errorMessage: 'Insira uma editora válida.' } },
+    quantidade: { trim: true, notEmpty: { errorMessage: 'Insira uma quantidade válida.' } }
+}), livroController.update);
+router.get('/admin/livro/delete/:id', livroController.delete);
+
+// Admin user CRUD
+router.get('/admin/users(/)?', userController.home);
+router.get('/admin/users/new', userController.new);
+router.post('/admin/users/new', checkSchema({
+    nome: { trim: true, notEmpty: { errorMessage: 'Insira um nome válido.' } },
+    email: { trim: true, isEmail: { errorMessage: 'Insira um email válido.' } },
+    senha: { notEmpty: { errorMessage: 'Insira uma senha válida.' } }
+}), userController.create);
+router.get('/admin/users/edit/:id', userController.edit)
+router.post('/admin/users/edit/:id', checkSchema({
+    nome: { trim: true, notEmpty: { errorMessage: 'Insira um nome válido.' } },
+    email: { trim: true, isEmail: { errorMessage: 'Insira um email válido.' } },
+    senha: { notEmpty: { errorMessage: 'Insira uma senha válida.' } }
+}), userController.update);
+router.get('/admin/users/delete/:id', userController.delete);
 
 module.exports = router;
